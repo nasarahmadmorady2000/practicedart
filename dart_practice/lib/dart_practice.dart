d9300e1c25fd4e1250eff4cb58dca266cf8ab328
Nesarahmad <nasarahmemo12345@gmail.com>
2026-06-04 13:41:28 +0430

    Initial commit

---
+++ dart_practice/lib/dart_practice.dart
@@ -0,0 +1,28 @@
+main() {
+  // int calculate() {
+  //   return 9 * 7;
+  // }
+
+  // var ahmad = "this is my teacher name";
+  // var num = ahmad;
+  // var condition = 6 > 7;
+  // if (condition) {
+  //   print("this is true");
+
+  //   print(
+  //     'The answer to the Ultimate Question of Life, The Universe, and Everything is ${calculate()}.',
+  //   );
+  // } else {
+  //   print(num);
+  // }
+  // int i = 0;
+  // while (i < 5) {
+  //   print(i);
+  //   i++;
+  // }
+  for (var i = 10; i < 10; i++) {
+    for (var n = 0; n < 5; n++) {
+      print("*");
+    }
+  }
+}
