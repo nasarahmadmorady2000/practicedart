7646c2560e656df971620eed9fe2dc1c954ac70d
Nesarahmad <nasarahmemo12345@gmail.com>
2026-06-05 08:29:33 +0430

    the second day of working on dart

--- dart_practice/bin/dart_practice.dart
+++ dart_practice/bin/dart_practice.dart
@@ -1,8 +1,74 @@
 //import 'package:dart_practice/dart_practice.dart' as dart_practice;
 
+import 'dart:io';
+
 void main(List<String> arguments) {
   // print('Hello world: ${dart_practice.calculate()}!');
   // for (int i = 0; i <= 20; i += 2) {
   //   print(i);
   // }
+  // int sum = 0;
+  // for (int i = 0; i <= 100; i++) {
+  //   sum += i;
+  //   // print("The sum of the first $i natural numbers is: $sum");
+  // }
+  // print(sum);
+  // for (int i = 0; i < 5; i++) {
+  //   for (int j = 0; j < 5; j++) {
+  //     print("*");
+  //   }
+  //   print("\n");
+  // }
+  // for (int i = 0; i <= 10; i++) {
+  //   print("5 *$i=${5 * i}");
+  // }
+  // int maxTable = 10;
+  // int maxMultiply = 10;
+
+  // for (int j = 1; j <= maxMultiply; j++) {
+  //   for (int i = 1; i <= maxTable; i++) {
+  //     // print each row of all tables
+  //     stdout.write("${i}x$j=${i * j}\t");
+  //   }
+  //   print(""); // new line after each row
+  // }
+  //   int size = 10;
+
+  //   // header row
+  //   stdout.write("     ");
+  //   for (int j = 1; j <= size; j++) {
+  //     stdout.write("${j.toString().padLeft(4)}");
+  //   }
+  //   print("");
+
+  //   for (int i = 1; i <= size; i++) {
+  //     stdout.write("${i.toString().padLeft(4)}");
+
+  //     for (int j = 1; j <= size; j++) {
+  //       stdout.write("${(i * j).toString().padLeft(4)}");
+  //     }
+  //     print("");
+  //   }
+  // stdout.write("this is my teacher name");
+
+  // stdout.write("i belive it is true");
+  // int a = 0;
+  // int b = 1;
+  // for (int i = 0; i < 10; i++) {
+  //   stdout.write("$a ");
+  //   int temp = a;
+  //   a = b;
+  //   b = temp + b;
+  // }
+  //   int x = 0;
+  //   int y = 1;
+  //   for (int i = 0; i < 30; i++) {
+  //     stdout.write("$x ");
+  //     if (i == 10 || i == 20 || i == 30) {
+  //       print("");
+  //     }
+  //     int temp = x;
+  //     x = y;
+  //     y = temp + y;
+  //   }
 }
