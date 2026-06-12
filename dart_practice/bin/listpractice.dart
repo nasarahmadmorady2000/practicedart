1f114a6fbf497fce8edea026ba541e175c5fd3ba
Nesarahmad <nasarahmemo12345@gmail.com>
2026-06-12 12:54:49 +0430

    working on map method and classes

---
+++ dart_practice/bin/listpractice.dart
@@ -0,0 +1,114 @@
+import 'dart:io';
+// void main (List <String> args){
+//   var student=<student>[];
+//   student.add("ali");
+//   student.add("mahmood");
+//   for(int i=0; i<= student.lenth; i++){
+//     stdout.write()
+//   }
+// }
+
+  // List<String> names=["ali","mahmood","abdulmalik"];
+  // for(var name in names){
+  //   print(name);
+  // }
+  // List<int> numbers=[];
+  // numbers.add(3);
+  // numbers.add(5);
+  // numbers.add(9);
+  // print(numbers);
+  // List <String> frutes=[];
+  // for(int i=0; i<=3; i++){
+  //   stdout.write('enter your frute ${i+1}:');
+  //   String frute=stdin.readLineSync()!;
+  //   frutes.add(frute);
+  // }
+  // print('your frute');
+  // print(frutes);
+  // List<String> names=['ali','sara','mahmood'];
+  // String search='ali';
+  // if(names.contains(search)){
+  //   print('$search is located in names');
+  // }
+  // else{
+  //   print('$search is not located in names');
+  // }
+  // List<int> numbers=[3,4,5,6];
+  // print(numbers);
+  // numbers.remove(3);
+  // print(numbers);
+
+  // List<Student> student=[];
+  // student.add(Student('ali',3,'A'));
+  // student.add(Student('mahmood',8,'B'));
+  // student.add(Student('melad',5,'C'));
+  // student.add(Student('melad',5,'C'));
+  // for(var s in student){
+  //   print('${s.name}, ${s.age},${s.grade}');
+  // }
+
+  // List<String> names=['ali','milad','kabir'];
+  // for(int i=0; i<names.length;i++){
+  //   print('${i+1 }: ${names[i]}');
+  // }
+
+  // List<int> numbers=[23,40,34,21,56,76,23,98,67,56];
+  // var evenumber= numbers.where((n)=>n%2!=0).toList();
+  // print(evenumber);
+
+  // List<int> numbers=[45,34,23,56,87,54,67,34,76,43];
+  // var num=numbers.where((n)=>n%2==0).toList();
+  // // print(num);
+  // for(int i=0; i<=numbers.length; i++){
+  //   if(numbers[i]%2 !=0){
+  //     print("${numbers[i]} is an odd number");
+  //   }
+  //   else{
+  //     print("${numbers[i]} is even numbers");
+  //   }
+//   // }
+//   for(var num in numbers){
+//     if(num%2 !=0){
+// print("$num is and odd number");
+//     }
+//     else{
+//       print("$num is an even number");
+//     }
+//   }
+
+  // class Student{
+  //   String name;
+  //   int age;
+  //   String grade;
+  //   Student(this.name,this.age,this.grade);
+  // }
+
+  // int sum=0;
+  // for(var snum in numbers){
+  //   sum += snum;
+  // }
+  // print(sum);
+
+  // List<int> numbers=[45,34,23,56,87,54,67,34,76,43];
+  // int largest =numbers[0];
+  // for (var large in numbers){
+  //   if (largest <large ){
+  //     largest =large;
+  //   }
+  // }
+  // print(largest);
+// void main(List <String> args) {
+//   Map<itn,Student> student={};
+//   student[1]=Student(1,"ahmad",23,"male"),
+//   student[2]=Student(2,"ali",34,"male"),
+//   studetn[3]=Studet(3,"zahra",25,"femail")
+//   print(student.length);
+//
+// }
+// class Student{
+//   int id;
+//   String name;
+//   int age;
+//   String gender;
+//   Student(this.id=id, this.name=name, this.age=age, this.gender=gender);
+// }
